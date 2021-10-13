273ea3ca9b39a4e4a0eff4a40e4c27e74714dd4e
Harvey Schneider <50023408+shimthal61@users.noreply.github.com>
2021-10-13 21:56:16 +0100

    Guide to recoding and separating variables, and turning them into factors

---
+++ recoding_variables_script.R
@@ -0,0 +1,17 @@
+my_messy_data <- read_csv("https://raw.githubusercontent.com/ajstewartlang/03_data_wrangling/master/data/my_data.csv")
+
+head(my_messy_data)
+
+#Recode our four conditions
+my_messy_data %>%
+  mutate(condition = recode(condition,
+                            "1" = "PrimeA_TargetB",
+                            "2" = "PrimeA_TargetB",
+                            "3" = "PrimeB_TargetA",
+                            "4" = "PrimeB_TargetB")) %>%
+
+#Seperating our condition columns
+  separate(col = "condition", into = c("Prime", "Target"), sep = "_") %>%
+
+#Mutate our condition columns into factors
+  mutate(Prime = factor(Prime), Target = factor(Target)) %>%
